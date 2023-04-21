4fde6ff358908b397dc1ea073899ec21ebcb1e5c
Anubhav <anubhavpal15june@gmail.com>
2023-04-21 19:27:26 +0530

    Project sectup files commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+
+import { logo } from './assets'
+import { Home, CreatePost } from './Components'
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <header className='w-full flex justify' ></header>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
